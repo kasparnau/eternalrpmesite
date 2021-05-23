6e11a94c4aedf1881e1de1045a1403fe2e889231
Klicer <klicerfn@gmail.com>
2021-05-23 23:22:51 +0300

    Big site revamp - add actual content

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 import "./App.scss";
-import { motion } from "framer-motion";
+import { motion, useTransform, useViewportScroll } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+
+import "./img/background.png";
+import "./img/section_padding.png";
+import "./img/section_bg_purple.png";
+import "./img/feature_cars.png";
+import "./img/feature_apartments.png";
+import "./img/feature_performance.png";
+import "./img/feature_mdt.png";
+import "./img/feature_criminals.png";
+import "./img/feature_factions.png";
+
+import React from "react";
+
+const discordInvite = `https://discord.gg/geUvTyn55S`;
 
 const Arrow = () => {
   return (
@@ -30,7 +45,7 @@ const Link = ({ href, text }) => {
       <motion.div
         whileHover={{
           opacity: 0.6,
-          transition: { duration: 0.2 },
+          transition: { duration: 0.1 },
         }}
         whileTap={{ scale: 0.95 }}
       >
@@ -41,27 +56,205 @@ const Link = ({ href, text }) => {
   );
 };
 
-function App() {
+const SectionPadding = (props) => {
   return (
-    <div className="App">
+    <div className="SectionPadding" style={{ ...props.style }}>
+      {props.children}
+    </div>
+  );
+};
+
+const TitleSection = () => {
+  return (
+    <div
+      className="Background"
+      style={{
+        width: "100%",
+      }}
+    >
       <motion.div
         className="Title"
-        initial={{ scale: 0.5 }}
-        animate={{ scale: 1 }}
-        transition={{ duration: 0.5 }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1.0 }}
       >
         <div className="Header">
           <div className="Gradient">Eternal</div>
           <div className="Default">RP</div>
         </div>
         <div className="Description">
-          A fully made-from-scratch FiveM roleplay server. No ESX. <br />
-          No more buggy 2017 code.
+          A fully made-from-scratch FiveM roleplay server. Daily updates, great
+          performance and countless features!
         </div>
         <div className="Links">
-          <Link href="https://discord.gg/geUvTyn55S" text="Join our discord" />
+          <Link href={discordInvite} text="Join our discord" />
         </div>
       </motion.div>
+    </div>
+  );
+};
+
+const Offer = (props) => {
+  return (
+    <div
+      className="Offer"
+      style={{
+        ...props.style,
+      }}
+    >
+      <div
+        style={{
+          padding: "8px",
+        }}
+      >
+        <div className="OfferImage">
+          <div
+            className={props.image}
+            style={{
+              height: "100%",
+              width: "100%",
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+            }}
+          />
+        </div>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          padding: "8px",
+          width: "100%",
+        }}
+      >
+        <div style={{ width: "100%" }} className="OfferTitle">
+          {props.title}
+        </div>
+        <div style={{ marginTop: "8px" }} className="OfferDesc">
+          {props.desc}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const WhatWeOfferSection = () => {
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+    rootMargin: "-100px 0px",
+  });
+
+  return (
+    <div className="Section">
+      <motion.div
+        className="Container"
+        ref={ref}
+        animate={{ opacity: inView ? 1 : 0 }}
+        initial={{ opacity: 0 }}
+        transition={{ duration: 0.5 }}
+      >
+        <div className="SectionTitle">TOP FEATURES</div>
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "space-between",
+          }}
+        >
+          <Offer
+            image="image-cars"
+            title={`100+ CUSTOM & MANUALLY TUNED VEHICLES`}
+            desc={`Our city has a wide selection of custom vehicles. Each and every vehicle is manually tuned for realism and balance by our expert developer. Vehicles are divided into classes (S, A, B, C, etc) to ensure balance for chases and races!`}
+          />
+          <Offer
+            image="image-apartments"
+            title={`1000+ PROPERTIES`}
+            desc={`Almost every apartment and house in the city is able to be purchased and owned by a player. If you wish to play in the criminal side of the city, you can also rob them. ;)`}
+          />
+          <Offer
+            image="image-performance"
+            title={`BEST PERFORMANCE`}
+            desc={`Thanks to our server being developed from scratch by a great developer with years of experience, you can always expect high framerates and low latency, even on a potato! We can achieve this basically by not being idiots and actually making our server instead of copy pasting ESX.`}
+          />
+          <Offer
+            image="image-criminals"
+            title={`COUNTLESS CRIMINAL ACTIVITIES`}
+            desc={`Our city offers 50+ unique, engaging and fun activities ranging from petty crime to full on heists, we have something for every tier of criminals! We have skill based mini-games so you actually have to practice a bit. Find out more in RP ;)`}
+          />
+          <Offer
+            image="image-mdt"
+            title={`LAW ENFORCEMENT SYSTEMS`}
+            desc={`We always work hard to make our UI intuitive and great to use. Law enforcement are equipped with a modern, well designed and intuitive MDT system, enabling for complex and in-depth RP!`}
+          />
+          <Offer
+            image="image-factions"
+            title={`BUSINESS SYSTEMS`}
+            desc={`Creating a business is fast & easy. Take advantage of our factions system to start your own legal business, crime organization or even a law enforcement firm. Hire members, purchase a safe, purchase a garage, purchase a business bank account, or anything else! Your creativity is the limit.`}
+          />
+        </div>
+      </motion.div>
+    </div>
+  );
+};
+
+const AboutUsSection = () => {
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+    rootMargin: "-100px 0px",
+  });
+
+  return (
+    <div className="Section">
+      <motion.div
+        className="Container"
+        ref={ref}
+        animate={{ opacity: inView ? 1 : 0 }}
+        initial={{ opacity: 0 }}
+        transition={{ duration: 0.5 }}
+      >
+        <div className="SectionTitle">ABOUT US</div>
+        <div
+          className="About"
+          style={{ marginTop: "32px", width: "100%", maxWidth: "500px" }}
+        >
+          EternalRP was created, because there is only like 3 good roleplay
+          servers in all of FiveM. My love for game development and hate for ESX
+          motivated me to make a fun non pay2win server from scratch.
+          <br />
+        </div>
+        <div className="AboutButton">
+          <a className="Link" href={discordInvite}>
+            <motion.div
+              style={{
+                backgroundColor: "var(--primary)",
+                padding: "12px",
+                borderRadius: "4px",
+              }}
+              whileHover={{
+                opacity: 0.6,
+                transition: { duration: 0.1 },
+              }}
+              whileTap={{ scale: 0.95 }}
+            >
+              Join our discord
+              <Arrow />
+            </motion.div>
+          </a>
+        </div>
+      </motion.div>
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <div className="App">
+      <SectionPadding />
+      <TitleSection />
+      <SectionPadding />
+      <WhatWeOfferSection />
+      <SectionPadding />
+      <AboutUsSection />
     </div>
   );
 }
